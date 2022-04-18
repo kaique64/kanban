5f51306b496d2ad1fd0a7b69e97e20413308523b
kaique64 <kaiquehenrique.2005@outlook.com>
2022-04-18 11:46:43 -0300

    feat: add database connection

--- src/infra/webserver/server.ts
+++ src/infra/webserver/server.ts
@@ -1,5 +1,7 @@
-import * as express from 'express';
+import express from 'express';
 import 'dotenv/config';
+
+import '../orm/typeorm';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
