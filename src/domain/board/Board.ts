fb8250bc679623c448e76b784bf65aefd8d5d836
= <kaiquehenrique.2005@outlook.com>
2022-04-20 18:21:26 -0300

    feat: remove property of tasks from board entity

--- src/domain/board/Board.ts
+++ src/domain/board/Board.ts
@@ -3,16 +3,11 @@ import Task from "../task/Task";
 class Board {
 
     private name: string;
-    private tasks: Task[];
 
     constructor(name: string) {
         this.name = name;
     }
-
-    public addTask(name: string, description: string): void {
-        this.tasks.push(new Task(name, description, 'Low'));
-    }
-
+    
 }
 
 export default Board;
