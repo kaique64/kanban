8dcc4768dd33055e0ce6b3549634bb5f916ea068
= <kaiquehenrique.2005@outlook.com>
2022-04-19 22:36:10 -0300

    feat: create task data transfer object

--- src/infra/repositories/task/TaskRepositoryInMemory.ts
+++ src/infra/repositories/task/TaskRepositoryInMemory.ts
@@ -1,12 +1,13 @@
 import ITaskRepository from '../../../domain/task/ITaskRepository'
 import Task from '../../../domain/task/Task';
+import TaskDTO from '../../../domain/task/TaskDTO';
 
 class TaskRepositoryInMemory implements ITaskRepository {
 
     private tasks: Task[] = [];
 
-    public async create(task: Task): Promise<void> {
-        this.tasks.push(task);
+    public async create(data: TaskDTO): Promise<void> {
+        this.tasks.push(new Task(data.name, data.description, data.priority));
     }
     
     public async findByName(name: string): Promise<Task | undefined> {
