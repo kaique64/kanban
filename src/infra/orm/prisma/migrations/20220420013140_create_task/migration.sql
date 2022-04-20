627cdfe1b215c104c035e2ba8baca64abbb52462
= <kaiquehenrique.2005@outlook.com>
2022-04-19 22:34:18 -0300

    feat: add priority field

--- src/infra/orm/prisma/migrations/20220420013140_create_task/migration.sql
+++ src/infra/orm/prisma/migrations/20220420013140_create_task/migration.sql
@@ -1,17 +1,9 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Task` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Task`;
-
 -- CreateTable
 CREATE TABLE `task` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `priority` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
