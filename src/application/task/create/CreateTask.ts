bcd25e66bd63c04193128f13195b356de087ef1e
= <kaiquehenrique.2005@outlook.com>
2022-04-19 23:41:13 -0300

    feat: replace create task dto for task dto

--- src/application/task/create/CreateTask.ts
+++ src/application/task/create/CreateTask.ts
@@ -1,6 +1,7 @@
-import CreateTaskDTO from './CreateTaskDTO';
 import Task from '../../../domain/task/Task';
 import ITaskRepository from '../../../domain/task/ITaskRepository';
+import TPriority from '../../../domain/task/TPriority';
+import TaskDTO from '../../../domain/task/TaskDTO';
 
 class CreateTask {
     
@@ -10,10 +11,14 @@ class CreateTask {
         this.taskRepository = taskRepositoryInMemory;
     }
 
-    public execute(data: CreateTaskDTO): void {
-        const newTask: Task = data.createTask();
+    public async execute(data: TaskDTO): Promise<Task> {
+        const task = await this.taskRepository.create({
+            name: data.name,
+            description: data.description,
+            priority: data.priority as TPriority,
+        });
 
-        this.taskRepository.create(newTask);
+        return task;
     }
 
 }
