74a657fef285467c9f8d5f259b9d81d1e909d456
= <kaiquehenrique.2005@outlook.com>
2022-04-21 10:18:14 -0300

    feat: create board controller

---
+++ src/infra/http/board/controllers/BoardController.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import BoardRepositoryWithPrismaORM from "src/infra/repositories/board/BoardRepositoryWithPrismaORM";
+
+const prismaClient = new PrismaClient();
+const boardRepository = new BoardRepositoryWithPrismaORM(prismaClient);
+
+class BoardController {
+
+    public async create(req: Request, res: Response): Promise<Response> {
+        const { name } = req.body;
+
+        const board = await boardRepository.create({ name });
+
+        return res.status(200).json(board);
+    }
+
+}
+
+export default BoardController;
