84b427e4fcd0a9d8bc3c1c51c59a5140964a8298
= <kaiquehenrique.2005@outlook.com>
2022-04-21 12:11:10 -0300

    feat: add celebrate error middleware to handle the validation error

--- src/infra/webserver/server.ts
+++ src/infra/webserver/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -8,5 +9,6 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(PORT, () => console.log(`Server is running! Access on http://localhost:${PORT}`));
