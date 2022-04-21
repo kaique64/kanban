3a7d39c9cd483955f1e7d7f6865979c7d30b44c0
= <kaiquehenrique.2005@outlook.com>
2022-04-21 09:03:01 -0300

    feat: create table board

--- src/infra/orm/prisma/migrations/20220421115440_create_board/migration.sql
+++ src/infra/orm/prisma/migrations/20220421115440_create_board/migration.sql
@@ -1,11 +1,10 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `Board` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
 
-  - You are about to drop the `task` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `task`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Task` (
@@ -13,8 +12,12 @@ CREATE TABLE `Task` (
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `priority` VARCHAR(191) NOT NULL,
+    `boardId` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Task` ADD CONSTRAINT `Task_boardId_fkey` FOREIGN KEY (`boardId`) REFERENCES `Board`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
