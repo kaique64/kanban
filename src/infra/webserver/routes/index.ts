fd20502e4d697038b541e5f492ceefee3204f838
= <kaiquehenrique.2005@outlook.com>
2022-04-21 11:26:20 -0300

    feat: add task routes to global application routes

--- src/infra/webserver/routes/index.ts
+++ src/infra/webserver/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import taskRoutes from "../../../infra/http/task/routes/task.routes";
 import boardRoutes from "../../http/board/routes/board.routes";
 
 const routes = Router();
 
 routes.use('/board', boardRoutes);
+routes.use('/task', taskRoutes);
 
 export default routes;
