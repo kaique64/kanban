e632190a404b58995404b5a50507573c74bf7d4f
= <kaiquehenrique.2005@outlook.com>
2022-04-21 10:03:37 -0300

    feat: inject board repository dependency and board not found exception

--- src/application/task/create/CreateTask.ts
+++ src/application/task/create/CreateTask.ts
@@ -2,16 +2,29 @@ import Task from '../../../domain/task/Task';
 import ITaskRepository from '../../../domain/task/ITaskRepository';
 import TPriority from '../../../domain/task/TPriority';
 import TaskDTO from '../../../domain/task/TaskDTO';
+import IBoardRepository from '../../../domain/board/IBoardRepository';
+import BoardNotFound from '../../../domain/board/BoardNotFound';
 
 class CreateTask {
     
     private readonly taskRepository: ITaskRepository;
+    private readonly boardRepository: IBoardRepository;
 
-    constructor(taskRepositoryInMemory: ITaskRepository) {
+    constructor(
+        taskRepositoryInMemory: ITaskRepository, 
+        boardRepository: IBoardRepository,
+    ) {
         this.taskRepository = taskRepositoryInMemory;
+        this.boardRepository = boardRepository;
     }
 
     public async execute(data: TaskDTO): Promise<Task> {
+        const board = await this.boardRepository.findById(data.boardId);
+
+        if (!board) {
+            throw new BoardNotFound(data.boardId);
+        }
+
         const task = await this.taskRepository.create({
             name: data.name,
             description: data.description,
