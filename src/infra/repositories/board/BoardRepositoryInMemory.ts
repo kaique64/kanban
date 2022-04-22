324960cf4cee6f7b33d199aaed6e2a25fb19a33d
kaique64 <kaiquehenrique.2005@outlook.com>
2022-04-22 15:06:11 -0300

    feat: add id to create function in board repository in memory

--- src/infra/repositories/board/BoardRepositoryInMemory.ts
+++ src/infra/repositories/board/BoardRepositoryInMemory.ts
@@ -1,5 +1,7 @@
+import BoardDTO from "src/domain/board/BoardDTO";
 import Board from "../../../domain/board/Board";
 import IBoardRepository from "../../../domain/board/IBoardRepository";
+import { v4 as uuid } from 'uuid';
 
 class BoardRepositoryInMemory implements IBoardRepository {
     
@@ -11,7 +13,7 @@ class BoardRepositoryInMemory implements IBoardRepository {
      * @return {Board} return board entity from domain
     */
     public async create(data: BoardDTO): Promise<Board> {
-        const board = new Board(data.name);
+        const board = new Board(data.name, uuid());
         
         this.boards.push(board);
 
