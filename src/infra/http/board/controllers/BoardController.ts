86a3de42928277934e52f23f74dd9fa894c301d7
= <kaiquehenrique.2005@outlook.com>
2022-04-22 20:55:12 -0300

    feat: add the api response to board controller

--- src/infra/http/board/controllers/BoardController.ts
+++ src/infra/http/board/controllers/BoardController.ts
@@ -1,14 +1,15 @@
-import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import CreateBoard from "../../../../application/board/create/CreateBoard";
 import BoardRepositoryWithPrismaORM from "../../../../infra/repositories/board/BoardRepositoryWithPrismaORM";
+import IErrorResponse from "../../pattern/http_response/IErrorResponse";
+import ISuccessResponse from "../../pattern/http_response/ISuccessResponse";
 
 const boardRepository = new BoardRepositoryWithPrismaORM();
 const createBoardUseCase = new CreateBoard(boardRepository);
 
 class BoardController {
 
-    public async create(req: Request, res: Response): Promise<Response> {
+    public async create(req: Request, res: Response<ISuccessResponse<{ name: string }> | IErrorResponse>): Promise<Response<ISuccessResponse<{ name: string }> | IErrorResponse> | undefined> {
         const { name } = req.body;
 
         const board = await createBoardUseCase.execute({ name });
