12295e81c431258d8b65494dcb3129f4f79ed235
= <kaiquehenrique.2005@outlook.com>
2022-04-22 18:42:45 -0300

    feat: add function to list tasks by board id in the task repository interface

--- src/infra/webserver/server.ts
+++ src/infra/webserver/server.ts
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 5001;
 
 // Connect database
 const prismaClient = new PrismaClient();
-prismaClient.$connect().then(() => console.log('MySQL connected'));
 prismaClient.$connect()
     .then(() => console.log('MySQL connected!'))
     .catch((err) => console.log(err));
