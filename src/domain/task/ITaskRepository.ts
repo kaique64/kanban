a8c63e972ed18ab49551d88d645f80f2c687d47d
kaique64 <kaiquehenrique.2005@outlook.com>
2022-04-25 11:23:02 -0300

    feat: add update function to task repository interface

--- src/domain/task/ITaskRepository.ts
+++ src/domain/task/ITaskRepository.ts
@@ -1,13 +1,16 @@
+import IUpdateTaskDTO from './IUpdateTaskDTO';
 import Task from './Task';
 import TaskDTO from './TaskDTO';
 
 interface ITaskRepository {
 
     create(data: TaskDTO): Promise<Task>;
+
+    update(data: IUpdateTaskDTO): Promise<void>;
     
     findById(id: string): Promise<Task | undefined>;
 
-    listTaskByBoard(board_id: string): Promise<Task[]>
+    listTaskByBoard(board_id: string): Promise<Task[]>;
 
 }
 
