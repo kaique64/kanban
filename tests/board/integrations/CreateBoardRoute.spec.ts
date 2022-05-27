63e8bc4814d86a4088d8e967bf6be02898932a29
kaique64 <kaiquehenrique.2005@outlook.com>
2022-05-27 14:04:45 -0300

    feat: create integration test

--- tests/board/integrations/CreateBoardRoute.spec.ts
+++ tests/board/integrations/CreateBoardRoute.spec.ts
@@ -1,12 +1,22 @@
-describe('Create Board Route', () => {
+import supertest from 'supertest';
 
-    beforeEach(() => {
-    });
+describe('Create Board Route - POST - /v1/board/create', () => {
 
     it('should be able to create a board', async () => {
+        const response = await supertest('http://localhost:5000').post('/v1/board/create').send({
+            name: 'To do',
+        });
+
+        expect(response.body.body).toHaveProperty('name', 'To do');
     });
 
     it('should be not able to create a board with non-existing name', async () => {
+        const response = await supertest('http://localhost:5000').post('/v1/board/create').send({
+            name: '',
+        });
+
+        expect(response.body).toHaveProperty('message', 'Validation failed');
+        expect(response.body.validation.body).toHaveProperty('message', "\"name\" is not allowed to be empty");
     });
 
 });
