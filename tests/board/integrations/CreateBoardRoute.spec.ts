d464c94c465b14302ea8881d9385895ecd5869ff
kaique64 <kaiquehenrique.2005@outlook.com>
2022-05-27 11:49:44 -0300

    test: create integration test file to board entity

---
+++ tests/board/integrations/CreateBoardRoute.spec.ts
@@ -0,0 +1,12 @@
+describe('Create Board Route', () => {
+
+    beforeEach(() => {
+    });
+
+    it('should be able to create a board', async () => {
+    });
+
+    it('should be not able to create a board with non-existing name', async () => {
+    });
+
+});
